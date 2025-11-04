d727bc1f2941f94ac56641ea587d53af7d8dffc8
費俊彥 <ericfei@feijunyandeMacBook-Pro.local>
2025-11-05 00:33:52 +0800

    add transfer transition and test

---
+++ util/random.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(letters)
+	for i := 0; i < n; i++ {
+		c := letters[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
